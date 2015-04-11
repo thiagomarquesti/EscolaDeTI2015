660deb439d0a14bcf34d6f2cb76d8152b743bc66
eduardobetioli <eduardo@betioli.com>
2015-04-11 11:29:18 -0300

    Manter usuário - frontend - Finalizado

--- src/main/java/br/unicesumar/time05/usuario/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.unicesumar.time05.usuario;
-
-import java.io.Serializable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
-}
