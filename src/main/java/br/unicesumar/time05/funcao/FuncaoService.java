da80de99ec5b05f71824aba23ffa5ea0ed66a534
jose.quintela <jp518@mail.com>
2015-06-13 15:11:14 -0300

    #6172 JosePaulo BackEnd de Funcao

---
+++ src/main/java/br/unicesumar/time05/funcao/FuncaoService.java
@@ -0,0 +1,52 @@
+package br.unicesumar.time05.funcao;
+
+import br.unicesumar.time05.rowMapper.MapRowMapper;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Component
+@Transactional
+class FuncaoService {
+    
+    @Autowired
+    private FuncaoRepository repo;
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+    
+    
+    void salvarFuncao(Funcao aFuncao) {
+        repo.save(aFuncao);
+    }
+
+    
+    public void alterarFuncao(Funcao aFuncao) {
+        repo.save(aFuncao);
+    }
+    
+    void removerFuncao(Long aIdFuncao) {
+        repo.delete(aIdFuncao);
+    }
+    public List<Map<String, Object>> getFuncoes() {
+
+        final String SQL = "SELECT idfuncao, descricao FROM FUNCAO";
+        List<Map<String, Object>> funcoes = jdbcTemplate.query(SQL, new MapRowMapper());
+        return Collections.unmodifiableList(funcoes);
+    }
+
+    public List<Map<String, Object>> getFuncaoById(Long aIdFuncao) {
+
+        final String SQL = "SELECT idfuncao, descricao FROM FUNCAO WHERE idfuncao = :idfuncao";
+        final MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("idfuncao", aIdFuncao);
+
+        List<Map<String, Object>> funcoes = jdbcTemplate.query(SQL, params, new MapRowMapper());
+        return Collections.unmodifiableList(funcoes);
+    }
+}
