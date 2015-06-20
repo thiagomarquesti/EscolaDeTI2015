a47053fcf7eef451ce4afdd754b2ef8c547ac680
Eduardo Betioli <eduardobetioli@gmail.com>
2015-06-20 16:06:16 -0300

    #6035 Eduardo Betioli - Copiando arquivos necessários

---
+++ src/main/webapp/js/CameraController.js
@@ -0,0 +1,17 @@
+(function(angular) {
+    'use strict';
+
+    angular
+        .module('camera')
+        .controller('cameraController', controller);
+
+    controller.$inject = [];
+
+    function controller() {
+        /* jshint validthis: true */
+        var vm = this;
+
+        vm.picture = false; // Initial state
+    }
+
+})(angular);
