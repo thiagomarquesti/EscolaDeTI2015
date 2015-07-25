61822214c6fb4f94cf3a29f1f9fab220c58c49ce
brunofiorentini <brunofiorentini93@gmail.com>
2015-07-25 14:26:34 -0300

    #6134 Bruno: Modificações no etnia e no indigena para corrigir erros

--- src/main/java/br/unicesumar/time05/indigena/Indigena.java
+++ src/main/java/br/unicesumar/time05/indigena/Indigena.java
@@ -21,6 +21,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Indigena implements Serializable {
