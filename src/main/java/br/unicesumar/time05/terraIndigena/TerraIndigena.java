b4985570b64c0d1e5054ca31f5a2b703b97ecc38
renato.alecrim <renato_alecrim@live.com>
2015-08-10 15:45:03 -0300

    #6491 Alecrim: criacao do controller e service do terra indigena

--- src/main/java/br/unicesumar/time05/terraIndigena/TerraIndigena.java
+++ src/main/java/br/unicesumar/time05/terraIndigena/TerraIndigena.java
@@ -5,18 +5,22 @@ import br.unicesumar.time05.cidade.Cidade;
 import br.unicesumar.time05.uf.UF;
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-@Entity
+@Entity(name = "terraindigena")
 public class TerraIndigena implements Serializable{
    @Id
    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+   @CampoConsulta
+   @Column(name = "idterraindigena")
    private Long idTerraIndigena;
    @CampoConsulta
+   @Column(name = "nometerra")
    private String nomeTerra;
    @ManyToOne
    private Cidade cidade;
--- src/main/java/br/unicesumar/time05/terraIndigena/TerraIndigenaService.java
+++ src/main/java/br/unicesumar/time05/terraIndigena/TerraIndigenaService.java
@@ -2,7 +2,11 @@ package br.unicesumar.time05.terraIndigena;
 
 import br.unicesumar.time05.ConsultaPersonalizada.ConstrutorDeSQL;
 import classesBase.ServiceBase;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Component;
 
+@Component
+@Transactional
 public class TerraIndigenaService extends ServiceBase<TerraIndigena, Long, TerraIndigenaRepository>{
 
     public TerraIndigenaService() {
