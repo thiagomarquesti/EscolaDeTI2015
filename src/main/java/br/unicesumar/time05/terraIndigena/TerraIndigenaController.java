f849abde660a974180975f2afa206ec182cbd4a0
renato.alecrim <renato_alecrim@live.com>
2015-08-10 12:27:54 -0300

    #6491 Alecrim: Cricacao do controller, service e repository de terra indigena

--- src/main/java/br/unicesumar/time05/terraIndigena/TerraIndigenaController.java
+++ src/main/java/br/unicesumar/time05/terraIndigena/TerraIndigenaController.java
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/terraIndigena")
-public class TerraIndigenaController extends ControllerBase<TerraIndigena, Long, TerraIndigenaService> {
+public class TerraIndigenaController extends ControllerBase<TerraIndigena, Long, TerraIndigenaService>{
+
 }
--- src/main/java/br/unicesumar/time05/terraIndigena/TerraIndigenaService.java
+++ src/main/java/br/unicesumar/time05/terraIndigena/TerraIndigenaService.java
@@ -3,7 +3,7 @@ package br.unicesumar.time05.terraIndigena;
 import br.unicesumar.time05.ConsultaPersonalizada.ConstrutorDeSQL;
 import classesBase.ServiceBase;
 
-class TerraIndigenaService extends ServiceBase<TerraIndigena, Long, TerraIndigenaRepository> {
+public class TerraIndigenaService extends ServiceBase<TerraIndigena, Long, TerraIndigenaRepository>{
 
     public TerraIndigenaService() {
         setConstrutorDeSQL(new ConstrutorDeSQL(TerraIndigena.class));
