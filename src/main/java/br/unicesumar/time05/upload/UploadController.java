d5f1cac383f892d5b6c634d94bb2270c6633c5e5
renato.alecrim <renato_alecrim@live.com>
2015-08-30 17:27:52 -0300

    #6621 Alecrim: simplificacao do codigo de salvar imagem e alteracao para que ele seja um service

--- src/main/java/br/unicesumar/time05/upload/UploadController.java
+++ src/main/java/br/unicesumar/time05/upload/UploadController.java
@@ -1,38 +1,22 @@
 package br.unicesumar.time05.upload;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import javax.imageio.stream.FileImageOutputStream;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import javax.transaction.Transactional;
 import org.apache.commons.codec.binary.Base64;
-import org.springframework.web.bind.annotation.RequestBody;
 
-@RestController
-@RequestMapping(value = "/upload")
+@Transactional
 public class UploadController {
 
-    @RequestMapping(method = RequestMethod.POST)
-    public void upload(@RequestBody String baseedString) throws IOException {
-//        System.out.println(baseedString);
-//        byte[] data = Base64.decodeBase64(baseedString);
-//        try (OutputStream stream = new FileOutputStream("C:/Users/Renato/Pictures/abc.bmp")) {
-//            stream.write(data);
-//        }
-        String stringCerta = baseedString.substring(baseedString.indexOf(",")+1);
+    public void upload(String baseedString, Long nomeArquivo) throws IOException {
+        String stringCerta = baseedString.substring(baseedString.indexOf(",") + 1);
         byte[] decode = Base64.decodeBase64(stringCerta);
-        int i = 5;
-        File f = new File("C:/Users/Renato/Pictures/" + i + ".jpg");
+        File f = new File("imagens/" + nomeArquivo + ".jpg");
+        
         FileOutputStream out = new FileOutputStream(f);
-        for (int x = 0; x < decode.length; x++) {
-            out.write(decode[x]);
-        }
+        out.write(decode);
         out.close();
-        System.out.println("passo");
     }
 
 }
