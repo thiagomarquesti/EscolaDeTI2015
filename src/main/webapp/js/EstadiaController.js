60961aeaa841c162b72900f81bdacd724f4a095a
renan.nakamura <nakamura_renan@hotmail.com>
2015-09-22 20:30:32 -0300

    #6730 Renan Nakamura - Finalizando tela de cadastro de estadia.

--- src/main/webapp/js/EstadiaController.js
+++ src/main/webapp/js/EstadiaController.js
@@ -5,23 +5,23 @@ module.controller("EstadiaController", ["$scope", "$http", "$routeParams", "$loc
         $scope.campoPrincipal = 'familia';
         $scope.isNovaEstadia = true;
 
-        $scope.atualizarListagens = function (qtdePorPag, pag, campo, string, troca, paro) {
-            if (campo == null || campo == "") {
-                campo = $scope.campoPrincipal;
-            }
-            $scope.dadosRecebidos = ServicePaginacao.atualizarListagens(qtdePorPag, pag, campo, string, $rootScope.ent, troca, paro);
-            atualizaScope;
-        };
+//        $scope.atualizarListagens = function (qtdePorPag, pag, campo, string, troca, paro) {
+//            if (campo == null || campo == "") {
+//                campo = $scope.campoPrincipal;
+//            }
+//            $scope.dadosRecebidos = ServicePaginacao.atualizarListagens(qtdePorPag, pag, campo, string, $rootScope.ent, troca, paro);
+//            atualizaScope;
+//        };
 
         function atualizaScope() {
             $scope = $rootScope;
         }
         $rootScope.atualizarListagens = $scope.atualizarListagens;
 
-        $scope.registrosPadrao = function () {
-            $scope.busca.numregistros = ServicePaginacao.registrosPadrao($scope.busca.numregistros);
-            $rootScope.numregistros = $scope.busca.numregistros;
-        };
+//        $scope.registrosPadrao = function () {
+//            $scope.busca.numregistros = ServicePaginacao.registrosPadrao($scope.busca.numregistros);
+//            $rootScope.numregistros = $scope.busca.numregistros;
+//        };
 
 
     }]);
