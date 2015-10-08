937f0841c471b7762e1334860baf6af031a7de9d
andrey.nakano <andreyhideki@gmail.com>
2015-10-07 22:33:25 -0300

    #6776 andrey - ajuda do Robson para gerar o PDF, falta implementar a parte de apresentar para o front e abrir em um nova janela o pdf

--- src/main/java/br/unicesumar/time05/indigena/IndigenaController.java
+++ src/main/java/br/unicesumar/time05/indigena/IndigenaController.java
@@ -2,8 +2,16 @@ package br.unicesumar.time05.indigena;
 
 import classesbase.ControllerBase;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -11,6 +19,8 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +29,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/indigena")
 public class IndigenaController extends ControllerBase<CriarIndigena, Long, IndigenaService> {
+    
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     public Indigena getObjeto(@PathVariable Long aId) {
@@ -29,41 +42,54 @@ public class IndigenaController extends ControllerBase<CriarIndigena, Long, Indi
     }
     
     @RequestMapping(value = "/rel", method = RequestMethod.GET)
-    public void verifcarDescricao() throws JRException {
+    public void verifcarDescricao(HttpServletResponse response) throws JRException, SQLException, IOException {
+        // compilacao do JRXML
+        URL reportResource = getClass().getClassLoader().getResource("./relatorios/teste01.jrxml");
+        JasperReport report = JasperCompileManager.compileReport(reportResource.getFile());  
+      
+        JasperPrint print = JasperFillManager.fillReport(report, new HashMap<String, Object>(), dataSource.getConnection());  
         
-        //System.out.println("Gerando relatório...");
-        //UsuarioDAO usuarioDAO = new UsuarioDAO();
-        //List listaUs = usuarioDAO.listaTodos();
-        String str = "br\\unicesumar\\time05\\relatorios\\teste02.jrxml";
-        //JasperReport xml = JasperCompileManager.compileReport(str);
-        //JasperReport pathjrxml = JasperCompileManager.compileReport("br/unicesumar/time05/relatorios/teste02.jrxml");
-        //JasperPrint printReport = JasperFillManager.fillReport(pathjrxml, null, new JRBeanCollectionDataSource(listaUs));
-        //JasperExportManager.exportReportToPdfFile(printReport, "relatorio/reportex.pdf");
-        //System.out.println("Relatorio gerado");
-                
-        List lista = new ArrayList();
-        //lista.add();
+        File pastaUsuario = new File(System.getProperty("user.dir"));
+        File pdf = new File(pastaUsuario, String.format("relatorioPessoas_%s.pdf", System.nanoTime()));
+        System.out.println("Imprimindo arquivo " + pdf);
+        JasperExportManager.exportReportToPdfFile(print, pdf.getAbsolutePath());
         
-        String caminho = "src/main/java/relatorios/teste01.jrxml";
-        // compilacao do JRXML 
-        JasperReport report = JasperCompileManager.compileReport(caminho);  
-        // preenchimento do relatorio, note que o metodo recebe 3 parametros: 
-        // 1 - o relatorio 
-        // 
-        // 2 - um Map, com parametros que sao passados ao relatorio 
-        // no momento do preenchimento. No nosso caso eh null, pois nao 
-        // estamos usando nenhum parametro 
-        // 
-        // 3 - o data source. Note que nao devemos passar a lista diretamente, 
-        // e sim "transformar" em um data source utilizando a classe 
-        // JRBeanCollectionDataSource 
-        JasperPrint print = JasperFillManager.fillReport(report, null, new JRBeanCollectionDataSource(lista));  
-        
-        // exportacao do relatorio para outro formato, no caso PDF 
-        JasperExportManager.exportReportToPdfFile(print, "src/main/java/relatorios/RelatorioPessoas.pdf");  
+        response.setHeader("Content-Type", "application/pdf");
+        ServletOutputStream outputStream = response.getOutputStream();
+        outputStream.write(IOUtils.toByteArray(new FileInputStream(pdf)));
+        outputStream.flush();
+        outputStream.close();
         
         System.out.println("Relatório gerado.");
- 
+
+// ******************************************************************
+        //Javascript no retorno para o front PDF
+//          function gerarRelatorioSuccess(response) {
+//            printReport(response.data);
+//          } 
+//
+//          function printReport(data) {
+//            var file = new Blob([ data ], {
+//              type : 'application/pdf'
+//            });
+//
+//            var url = URL.createObjectURL(file);
+//            window.open(url).print();
+//         }
+// ******************************************************************
+//          function gerarRelatorioSuccess(response) {
+//    printReport(response.data);
+//  } 
+//
+//  function printReport(data) {
+//    var file = new Blob([ data ], {
+//      type : 'text/html'
+//    });
+//
+//    var url = URL.createObjectURL(file);
+//    window.open(url).print();
+// }
+ // ******************************************************************      
     }
 
 }
