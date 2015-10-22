821a1bdd0406ed10a93e40b1d27850571b652320
thiagomarquesdacruz <thiago_marques179@hotmail.com>
2015-10-21 22:33:21 -0200

    #6896 - Thiago Cruz (Concluindo task)

--- src/main/java/br/unicesumar/time05/familia/FamiliaService.java
+++ src/main/java/br/unicesumar/time05/familia/FamiliaService.java
@@ -1,28 +1,14 @@
 package br.unicesumar.time05.familia;
 
 import br.unicesumar.time05.consultapersonalizada.ConstrutorDeSQL;
+import br.unicesumar.time05.relatorios.Relatorio;
+import br.unicesumar.time05.relatorios.formatoRelatorio;
 import br.unicesumar.time05.usuario.sessaousuario.SessaoUsuario;
 import classesbase.ServiceBase;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.ServletContext;
 import javax.sql.DataSource;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -33,10 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class FamiliaService extends ServiceBase<Familia, Long, FamiliaRepository> {
 
     @Autowired
-    DataSource dataSource;
-    
-    @Autowired
-    SessaoUsuario sessaoUsuario;
+    Relatorio relatorio;
 
     private final String sqlPadrao
             = "SELECT f.idfamilia, "
@@ -131,25 +114,7 @@ public class FamiliaService extends ServiceBase<Familia, Long, FamiliaRepository
         return query.execute(sql, params);
     }
 
-    String gerarRelatorioSimples() {
-
-        try {
-            URL reportResource = getClass().getClassLoader().getResource("./relatorios/teste01.jrxml");
-            JasperReport report = JasperCompileManager.compileReport(reportResource.getFile());
-            JasperPrint print = JasperFillManager.fillReport(report, new HashMap<String, Object>(), dataSource.getConnection());
-            
-            File path = new File("src/main/webapp/rels/" + sessaoUsuario.getUsuario().getIdUsuario());
-            path.mkdirs();
-            for (File arquivo : path.listFiles()){
-                arquivo.delete();
-            }
-            File pdf = new File(path + "/" + UUID.randomUUID().toString() + ".pdf");
-            JasperExportManager.exportReportToPdfFile(print, pdf.getAbsolutePath());
-            System.out.println("Imprimindo arquivo " + pdf);
-            return "/rels/" + sessaoUsuario.getUsuario().getIdUsuario() + "/" +  pdf.getName();
-        } catch (JRException | SQLException ex) {
-            Logger.getLogger(FamiliaService.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
+    public String gerarRelatorioSimples() {
+        return relatorio.gerarRelatorio("teste01.jrxml", formatoRelatorio.EXCEL);
     }
 }
