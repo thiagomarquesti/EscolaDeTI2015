8d1bdc3b48aa8442d1e185b2cdc8d9fef360c321
andrey.nakano <andreyhideki@gmail.com>
2015-11-07 14:56:19 -0200

    #6999 andrey - alterado o backend para retornar os aniversarios de pessoas fisicas

--- src/main/java/br/unicesumar/time05/eventos/EventosController.java
+++ src/main/java/br/unicesumar/time05/eventos/EventosController.java
@@ -9,6 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/eventos")
 public class EventosController extends ControllerBase<Eventos, Long, EventosService> {
-    
-    
+
 }
--- src/main/java/br/unicesumar/time05/eventos/EventosService.java
+++ src/main/java/br/unicesumar/time05/eventos/EventosService.java
@@ -8,11 +8,33 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-@Transactional 
+@Transactional
 public class EventosService extends ServiceBase<Eventos, Long, EventosRepository> {
+
+    private final String sql
+            = "      select e.descricao, "
+            + " 	    e.horainicial, "
+            + " 	    e.horafinal, "
+            + " 	    e.datainicial, "
+            + " 	    e.datafinal, "
+            + " 	    e.visualizarnocalendario, "
+            + "             'EVENTO' as tipo "
+            + "  from eventos e "
+            + " union all "
+            + "select 'Aniversário '||p.nome as descricao, "
+            + "       '00:00:00' as horainicial, "
+            + "       '23:59:00' as horafinal, "
+            + "       pf.datanascimento as datainicial, "
+            + "       pf.datanascimento as datafinal, "
+            + "       true as visualizarnocalendario, "
+            + "       'ANIVERSARIO' as tipo "
+            + "  from pessoa p "
+            + " inner join pessoa_fisica pf "
+            + "    on pf.idpessoa = p.idpessoa ";
 
     public EventosService() {
         setConstrutorDeSQL(new ConstrutorDeSQL(Eventos.class));
+        setSqlPadrao(sql, "descricao");
     }
-    
+
 }
