fd9a651c277ec7834f1d8ebe4d4eb988ffa34847
andrey.nakano <andreyhideki@gmail.com>
2015-11-07 15:12:39 -0200

    #6999 andrey - adicionado retorno para o calendario de indigenas

--- src/main/java/br/unicesumar/time05/eventos/EventosService.java
+++ src/main/java/br/unicesumar/time05/eventos/EventosService.java
@@ -30,7 +30,16 @@ public class EventosService extends ServiceBase<Eventos, Long, EventosRepository
             + "       'ANIVERSARIO' as tipo "
             + "  from pessoa p "
             + " inner join pessoa_fisica pf "
-            + "    on pf.idpessoa = p.idpessoa ";
+            + "    on pf.idpessoa = p.idpessoa "
+            + " union all "
+            + " select 'Aniversário '||i.nome as descricao, "
+            + "        '00:00:00' as horainicial, "
+            + "        '23:59:00' as horafinal, "
+            + "        i.datanascimento as datainicial, "
+            + "        i.datanascimento as datafinal, "
+            + "        true as visualizarnocalendario, "
+            + "        'ANIVERSARIO' as tipo "
+            + "   from indigena i ";
 
     public EventosService() {
         setConstrutorDeSQL(new ConstrutorDeSQL(Eventos.class));
