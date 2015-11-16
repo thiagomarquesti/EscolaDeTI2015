60dd02055574cac85483a0998cbc812d3582d596
andrey.nakano <andreyhideki@gmail.com>
2015-11-16 01:12:34 -0200

    #7022 andrey - criando rotina de envio de email

---
+++ src/main/java/br/unicesumar/time05/email/EnviaEmail.java
@@ -0,0 +1,55 @@
+package br.unicesumar.time05.email;
+
+public class EnviaEmail {
+    //email contatoassindi@gmail.com
+    //senha assindi123
+    private String username;
+    private String password;
+    private String endereco;
+    private String titulo;
+    private String conteudo;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public EnviaEmail() {
+    }
+    
+}
--- src/main/java/br/unicesumar/time05/usuario/sessaousuario/SessaoUsuarioController.java
+++ src/main/java/br/unicesumar/time05/usuario/sessaousuario/SessaoUsuarioController.java
@@ -3,6 +3,7 @@ package br.unicesumar.time05.usuario.sessaousuario;
 import br.unicesumar.time05.itemacesso.ItemAcessoUsuarioInMemory;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @RestController
 @RequestMapping(value = "/login")
@@ -41,5 +49,51 @@ public class SessaoUsuarioController {
     @RequestMapping(value = "/usuariologado/itensdeacesso", method = RequestMethod.GET)
     public ItemAcessoUsuarioInMemory getItensDeAcessoDoUsuarioLogado(){
         return service.getItensDeAcessoUsuarioLogado();
+    }
+    
+//    aEnviaEmail{
+//    "username";""
+//    "password";""
+//    "endereco";"andreyhideki@gmail.com"
+//    "titulo";"teste com parametro"
+//    "conteudo";"teste com parametro"
+//}
+    @RequestMapping(value = "/enviaemail",  method = RequestMethod.GET)
+    //@RequestMapping(value = "/enviaemail",  method = RequestMethod.POST)
+    //public void enviaemail(@RequestBody EnviaEmail aEnviaEmail){
+    public void enviaemail(){
+        final String username = "contatoassindi@gmail.com";
+        final String password = "assindi123";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                }
+          });
+
+        try {
+
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("from-email@gmail.com"));
+                message.setRecipients(Message.RecipientType.TO,
+                        InternetAddress.parse("andreyhideki@gmail.com"));
+                message.setSubject("TESTE Titulo");
+                message.setText("ASSINDI,"
+                        + "\n\n TESTEEEEEEEE!");
+
+                Transport.send(message);
+
+                //System.out.println("Done");
+
+        } catch (MessagingException e) {
+                throw new RuntimeException(e);
+        }
     }
 }
